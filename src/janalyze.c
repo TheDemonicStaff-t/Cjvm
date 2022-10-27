aa02c49c9f7e4fec8617f71047649a1fa72ccacb
SAM <thedemonicstaff@gmail.com>
2022-10-27 00:57:31 -0400

    successful parsing of constantpool

--- src/janalyze.c
+++ src/janalyze.c
@@ -54,28 +54,61 @@ int main(int argc, char** argv){
                 file.constpool[i].jconst._fieldref->name_and_type_index = jvm_u16(data[idx++], data[idx++]);
                 break;
             case CONSTANT_Methodref:
+                file.constpool[i].jconst._methodref = (constant_methodref*) malloc(sizeof(constant_methodref));
+                file.constpool[i].jconst._methodref->class_index = jvm_u16(data[idx++], data[idx++]);
+                file.constpool[i].jconst._methodref->name_and_type_index = jvm_u16(data[idx++], data[idx++]);
                 break;
             case CONSTANT_InterfaceMethodref:
+                file.constpool[i].jconst._interfacemethodref = (constant_interfacemethodref*) malloc(sizeof(constant_interfacemethodref));
+                file.constpool[i].jconst._interfacemethodref->class_index = jvm_u16(data[idx++], data[idx++]);
+                file.constpool[i].jconst._interfacemethodref->name_and_type_index = jvm_u16(data[idx++], data[idx++]);
                 break;
             case CONSTANT_String:
+                file.constpool[i].jconst._string = (constant_string*) malloc(sizeof(constant_string));
+                file.constpool[i].jconst._string->string_index = jvm_u16(data[idx++], data[idx++]);
                 break;
             case CONSTANT_Integer:
+                file.constpool[i].jconst._int = (constant_int*) malloc(sizeof(constant_int));
+                file.constpool[i].jconst._int->num = jvm_i32(data[idx++],data[idx++],data[idx++],data[idx++]);
                 break;
             case CONSTANT_Float:
+                file.constpool[i].jconst._float = (constant_float*) malloc(sizeof(constant_float));
+                file.constpool[i].jconst._float->num = jvm_f32(data[idx++],data[idx++],data[idx++],data[idx++]);
                 break;
             case CONSTANT_Long:
+                file.constpool[i].jconst._long = (constant_long*) malloc(sizeof(constant_long));
+                file.constpool[i].jconst._long->num = jvm_i64(data[idx++],data[idx++],data[idx++],data[idx++],data[idx++],data[idx++],data[idx++],data[idx++]);
                 break;
             case CONSTANT_Double:
+                file.constpool[i].jconst._double = (constant_double*) malloc(sizeof(constant_double));
+                file.constpool[i].jconst._double->num = jvm_f64(data[idx++],data[idx++],data[idx++],data[idx++],data[idx++],data[idx++],data[idx++],data[idx++]);
                 break;
             case CONSTANT_NameAndType:
+                file.constpool[i].jconst._nameandtype = (constant_nameandtype*) malloc(sizeof(constant_nameandtype));
+                file.constpool[i].jconst._nameandtype->name_index = jvm_u16(data[idx++], data[idx++]);
+                file.constpool[i].jconst._nameandtype->descriptor_index = jvm_u16(data[idx++], data[idx++]);
                 break;
             case CONSTANT_Utf8:
+                file.constpool[i].jconst._utf8 = (constant_utf8*) malloc(sizeof(constant_utf8));
+                file.constpool[i].jconst._utf8->length = jvm_u16(data[idx++], data[idx++]);
+                file.constpool[i].jconst._utf8->bytes = (char*) malloc(sizeof(char)*(file.constpool[i].jconst._utf8->length+1));
+                for (int x = 0; i < file.constpool[i].jconst._utf8->length; x++){
+                    file.constpool[i].jconst._utf8->bytes[x] = data[idx++];
+                } file.constpool[i].jconst._utf8->bytes[file.constpool[i].jconst._utf8->length+1] = '\0';
                 break;
             case CONSTANT_MethodHandle:
+                file.constpool[i].jconst._methodhandle = (constant_methodhandle*) malloc(sizeof(constant_methodhandle));
+                file.constpool[i].jconst._methodhandle->reference_kind = data[idx++];
+                file.constpool[i].jconst._methodhandle->reference_index = jvm_u16(data[idx++], data[idx++]);;
                 break;
             case CONSTANT_MethodType:
+                file.constpool[i].jconst._methodtype = (constant_methodtype*) malloc(sizeof(constant_methodtype));
+                file.constpool[i].jconst._methodtype->descriptor_index = jvm_u16(data[idx++], data[idx++]);
                 break;
             case CONSTANT_InvokeDynamic:
+                file.constpool[i].jconst._invokedynamic = (constant_invokedynamic*) malloc(sizeof(constant_invokedynamic));
+                file.constpool[i].jconst._invokedynamic->bootstrap_method_attr_index = jvm_u16(data[idx++], data[idx++]);
+                file.constpool[i].jconst._invokedynamic->name_and_type_index = jvm_u16(data[idx++], data[idx++]);
                 break;
             default:
                 printf("unknown constant");
@@ -105,20 +138,29 @@ int main(int argc, char** argv){
                 free(file.constpool[i].jconst._int);
                 break;
             case CONSTANT_Float:
+                free(file.constpool[i].jconst._float);
                 break;
             case CONSTANT_Long:
+                free(file.constpool[i].jconst._long);
                 break;
             case CONSTANT_Double:
+                free(file.constpool[i].jconst._double);
                 break;
             case CONSTANT_NameAndType:
+                free(file.constpool[i].jconst._nameandtype);
                 break;
             case CONSTANT_Utf8:
+                free(file.constpool[i].jconst._utf8->bytes);
+                free(file.constpool[i].jconst._utf8);
                 break;
             case CONSTANT_MethodHandle:
+                free(file.constpool[i].jconst._methodhandle);
                 break;
             case CONSTANT_MethodType:
+                free(file.constpool[i].jconst._methodtype);
                 break;
             case CONSTANT_InvokeDynamic:
+                free(file.constpool[i].jconst._invokedynamic);
                 break;
             default:
                 printf("unknown constant");
