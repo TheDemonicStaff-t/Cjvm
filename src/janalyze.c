ebf793e8c7540b01ae98ca45ee3d42932d716e1c
your_username <thedemonicstaff@gmail.com>
2022-10-26 22:04:45 -0400

    indexing constant pool and setting up freer

--- src/janalyze.c
+++ src/janalyze.c
@@ -30,10 +30,103 @@ int main(int argc, char** argv){
     jvm_class_file file;
 
     file.valid = 1;
+
+    // gen version
     f32 tmp = ((f32)jvm_u16(data[idx++], data[idx++])/100)+((f32)jvm_u16(data[idx++], data[idx++]));
     file.version = (char*) malloc(sizeof(char)*9);
     sprintf(file.version, "%f", tmp);
-    printf("%s\n", file.version);
+
+    file.constpool_c = jvm_u16(data[idx++], data[idx++]) - 1;
+
+    file.constpool = (jvm_constant *) malloc(sizeof(jvm_constant)*file.constpool_c);
+
+    for(int i = 0; i < file.constpool_c; i++){
+        file.constpool[i].tag = data[idx++];
+
+        switch (file.constpool[i].tag){
+            case CONSTANT_Class:
+                file.constpool[i].jconst._class = (constant_class*) malloc(sizeof(constant_class));
+                file.constpool[i].jconst._class->name_index = jvm_u16(data[idx++], data[idx++]);
+                break;
+            case CONSTANT_Fieldref:
+                file.constpool[i].jconst._fieldref = (constant_fieldref*) malloc(sizeof(constant_fieldref));
+                file.constpool[i].jconst._fieldref->class_index = jvm_u16(data[idx++], data[idx++]);
+                file.constpool[i].jconst._fieldref->name_and_type_index = jvm_u16(data[idx++], data[idx++]);
+                break;
+            case CONSTANT_Methodref:
+                break;
+            case CONSTANT_InterfaceMethodref:
+                break;
+            case CONSTANT_String:
+                break;
+            case CONSTANT_Integer:
+                break;
+            case CONSTANT_Float:
+                break;
+            case CONSTANT_Long:
+                break;
+            case CONSTANT_Double:
+                break;
+            case CONSTANT_NameAndType:
+                break;
+            case CONSTANT_Utf8:
+                break;
+            case CONSTANT_MethodHandle:
+                break;
+            case CONSTANT_MethodType:
+                break;
+            case CONSTANT_InvokeDynamic:
+                break;
+            default:
+                printf("unknown constant");
+                return 3;
+        }
+    }
+
+    for(int i = 0; i < file.constpool_c; i++){
+
+        switch (file.constpool[i].tag){
+            case CONSTANT_Class:
+                free(file.constpool[i].jconst._class);
+                break;
+            case CONSTANT_Fieldref:
+                free(file.constpool[i].jconst._fieldref);
+                break;
+            case CONSTANT_Methodref:
+                free(file.constpool[i].jconst._methodref);
+                break;
+            case CONSTANT_InterfaceMethodref:
+                free(file.constpool[i].jconst._interfacemethodref);
+                break;
+            case CONSTANT_String:
+                free(file.constpool[i].jconst._string);
+                break;
+            case CONSTANT_Integer:
+                free(file.constpool[i].jconst._int);
+                break;
+            case CONSTANT_Float:
+                break;
+            case CONSTANT_Long:
+                break;
+            case CONSTANT_Double:
+                break;
+            case CONSTANT_NameAndType:
+                break;
+            case CONSTANT_Utf8:
+                break;
+            case CONSTANT_MethodHandle:
+                break;
+            case CONSTANT_MethodType:
+                break;
+            case CONSTANT_InvokeDynamic:
+                break;
+            default:
+                printf("unknown constant");
+                return 3;
+        }
+    }
+
+    free(file.constpool);
 
     free(data);
 
