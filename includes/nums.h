0d9cf15c37ff249471251e4cea461f29e5f62d44
your_username <thedemonicstaff@gmail.com>
2022-10-27 09:32:58 -0400

    implemented parse print of constant pool

--- includes/nums.h
+++ includes/nums.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef NUMS_H
+#define NUMS_H
 
 #include <stdint.h>
 
@@ -16,24 +17,25 @@
 #define f64     double
 #define f128    long double
 
-u16 jvm_u16(u8 v1, u8 v2){return (((u16)v1)<<8)|(((u16)v2));}
-u32 jvm_u32(u8 v1, u8 v2, u8 v3, u8 v4){return (((u32)v1)<<24)|(((u32)v2)<<16)|(((u32)v3)<<8)|((u32)v4);}
-u64 jvm_u64(u8 v1, u8 v2, u8 v3, u8 v4, u8 v5, u8 v6, u8 v7, u8 v8){return (((u64)v1)<<56)|(((u64)v2)<<48)|(((u64)v3)<<40)|(((u64)v4)<<32)|(((u64)v5)<<24)|(((u64)v6)<<16)|(((u64)v7)<<8)|((u64)v8);}
+u16 jvm_u16(u8 v1, u8 v2);
+u32 jvm_u32(u8 v1, u8 v2, u8 v3, u8 v4);
+u64 jvm_u64(u8 v1, u8 v2, u8 v3, u8 v4, u8 v5, u8 v6, u8 v7, u8 v8);
 
-i16 jvm_i16(u8 v1, u8 v2){return (i16)(((u16)v1)<<8)|(((u16)v2));}
-i32 jvm_i32(u8 v1, u8 v2, u8 v3, u8 v4){return (i32)(((u32)v1)<<24)|(((u32)v2)<<16)|(((u32)v3)<<8)|(((u32)v4));}
-i64 jvm_i64(u8 v1, u8 v2, u8 v3, u8 v4, u8 v5, u8 v6, u8 v7, u8 v8){return (i64)((((u64)v1)<<56)|(((u64)v2)<<48)|(((u64)v3)<<40)|(((u64)v4)<<32)|(((u64)v5)<<24)|(((u64)v6)<<16)|(((u64)v7)<<8)|((u64)v8));}
+i16 jvm_i16(u8 v1, u8 v2);
+i32 jvm_i32(u8 v1, u8 v2, u8 v3, u8 v4);
+i64 jvm_i64(u8 v1, u8 v2, u8 v3, u8 v4, u8 v5, u8 v6, u8 v7, u8 v8);
 
-f32 jvm_f32(u8 v1, u8 v2, u8 v3, u8 v4){return (f32)((((u32)v1)<<24)|(((u32)v2)<<16)|(((u32)v3)<<8)|(((u32)v4)));}
-f64 jvm_f64(u8 v1, u8 v2, u8 v3, u8 v4, u8 v5, u8 v6, u8 v7, u8 v8){return (f64)((((u64)v1)<<56)|(((u64)v2)<<48)|(((u64)v3)<<40)|(((u64)v4)<<32)|(((u64)v5)<<24)|(((u64)v6)<<16)|(((u64)v7)<<8)|((u64)v8));}
+f32 jvm_f32(u8 v1, u8 v2, u8 v3, u8 v4);
+f64 jvm_f64(u8 v1, u8 v2, u8 v3, u8 v4, u8 v5, u8 v6, u8 v7, u8 v8);
 
-u16 nm_u16(u8 v1, u8 v2){return (u16)(((u16)v1)<<8)|(((u16)v2));}
-u32 nm_u32(u8 v1, u8 v2, u8 v3, u8 v4){return (((u32)v1)<<24)|(((u32)v2)<<16)|(((u32)v3)<<8)|(((u32)v4));}
-u64 nm_u64(u8 v1, u8 v2, u8 v3, u8 v4, u8 v5, u8 v6, u8 v7, u8 v8){return (u64)(((u64)v1)<<56)|(((u64)v2)<<48)|(((u64)v3)<<40)|(((u64)v4)<<32)|(((u64)v5)<<24)|(((u64)v6)<<16)|(((u64)v7)<<8)|((u64)v8);}
+u16 nm_u16(u8 v1, u8 v2);
+u32 nm_u32(u8 v1, u8 v2, u8 v3, u8 v4);
+u64 nm_u64(u8 v1, u8 v2, u8 v3, u8 v4, u8 v5, u8 v6, u8 v7, u8 v8);
 
-i16 nm_i16(u8 v1, u8 v2){return (i16)((((u16)v1)<<8)|(((u16)v2)));}
-i32 nm_i32(u8 v1, u8 v2, u8 v3, u8 v4){return (i32)((((u32)v1)<<24)|(((u32)v2)<<16)|(((u32)v3)<<8)|(((u32)v4)));}
-i64 nm_i64(u8 v1, u8 v2, u8 v3, u8 v4, u8 v5, u8 v6, u8 v7, u8 v8){return (i64)((((u64)v1)<<56)|(((u64)v2)<<48)|(((u64)v3)<<40)|(((u64)v4)<<32)|(((u64)v5)<<24)|(((u64)v6)<<16)|(((u64)v7)<<8)|((u64)v8));}
+i16 nm_i16(u8 v1, u8 v2);
+i32 nm_i32(u8 v1, u8 v2, u8 v3, u8 v4);
+i64 nm_i64(u8 v1, u8 v2, u8 v3, u8 v4, u8 v5, u8 v6, u8 v7, u8 v8);
 
-f32 nm_f32(u8 v1, u8 v2, u8 v3, u8 v4){return (f32)((((u32)v1)<<24)|(((u32)v2)<<16)|(((u32)v3)<<8)|(((u32)v4)));}
-f64 nm_f64(u8 v1, u8 v2, u8 v3, u8 v4, u8 v5, u8 v6, u8 v7, u8 v8){return (f64)((((u64)v1)<<56)|(((u64)v2)<<48)|(((u64)v3)<<40)|(((u64)v4)<<32)|(((u64)v5)<<24)|(((u64)v6)<<16)|(((u64)v7)<<8)|((u64)v8));}
+f32 nm_f32(u8 v1, u8 v2, u8 v3, u8 v4);
+f64 nm_f64(u8 v1, u8 v2, u8 v3, u8 v4, u8 v5, u8 v6, u8 v7, u8 v8);
+#endif
